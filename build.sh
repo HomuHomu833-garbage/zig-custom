c1a00417a06f734a9f0bc648e611ddbf02e6be79
Homura <egzozu.be1.bas@gmail.com>
2025-10-15 13:09:22 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -eu
 
-TARGET="$1"
-MCPU="$2"
+export ZIG_TARGET="$1"
 ROOTDIR="$(pwd)"
+TOOLCHAIN="$ROOTDIR/zig-as-llvm"
 ZIG_VERSION="$ZIG_VERSION_NAME"
 ZIG="$(whereis zig)"
 
@@ -18,49 +18,54 @@ case $TARGET_OS_CMAKE in
   native) TARGET_OS_CMAKE="";;
 esac
 
-mkdir -p "$ROOTDIR/out/build-zlib-$TARGET-$MCPU"
-cd "$ROOTDIR/out/build-zlib-$TARGET-$MCPU"
+git clone https://github.com/HomuHomu833/zig-as-llvm $TOOLCHAIN
+
+mkdir -p "$ROOTDIR/out/build-zlib-$ZIG_TARGET"
+cd "$ROOTDIR/out/build-zlib-$ZIG_TARGET"
 cmake "$ROOTDIR/zlib" \
-  -DCMAKE_INSTALL_PREFIX="$ROOTDIR/out/$TARGET-$MCPU" \
-  -DCMAKE_PREFIX_PATH="$ROOTDIR/out/$TARGET-$MCPU" \
+  -DCMAKE_INSTALL_PREFIX="$ROOTDIR/out/$ZIG_TARGET" \
+  -DCMAKE_PREFIX_PATH="$ROOTDIR/out/$ZIG_TARGET" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_CROSSCOMPILING=True \
   -DCMAKE_SYSTEM_NAME="$TARGET_OS_CMAKE" \
-  -DCMAKE_C_COMPILER="$ZIG;cc;-fno-sanitize=all;-s;-target;$TARGET;-mcpu=$MCPU" \
-  -DCMAKE_CXX_COMPILER="$ZIG;c++;-fno-sanitize=all;-s;-target;$TARGET;-mcpu=$MCPU" \
-  -DCMAKE_ASM_COMPILER="$ZIG;cc;-fno-sanitize=all;-s;-target;$TARGET;-mcpu=$MCPU" \
+  -DCMAKE_C_COMPILER="$TOOLCHAIN/bin/cc" \
+  -DCMAKE_CXX_COMPILER="$TOOLCHAIN/bin/c++" \
+  -DCMAKE_C_FLAGS="-fno-sanitize=all -s -mcpu=baseline"
+  -DCMAKE_CXX_FLAGS="-fno-sanitize=all -s -mcpu=baseline"
   -DCMAKE_LINK_DEPENDS_USE_LINKER=OFF \
-  -DCMAKE_RC_COMPILER="$ROOTDIR/out/host/bin/llvm-rc" \
-  -DCMAKE_AR="$ROOTDIR/out/host/bin/llvm-ar" \
-  -DCMAKE_RANLIB="$ROOTDIR/out/host/bin/llvm-ranlib"
+  -DCMAKE_RC_COMPILER="$TOOLCHAIN/bin/rc" \
+  -DCMAKE_AR="$TOOLCHAIN/bin/ar" \
+  -DCMAKE_RANLIB="$TOOLCHAIN/bin/ranlib"
 cmake --build . --target install
 
-mkdir -p "$ROOTDIR/out/$TARGET-$MCPU/lib"
-cp "$ROOTDIR/zstd/lib/zstd.h" "$ROOTDIR/out/$TARGET-$MCPU/include/zstd.h"
-cd "$ROOTDIR/out/$TARGET-$MCPU/lib"
+mkdir -p "$ROOTDIR/out/$ZIG_TARGET/lib"
+cp "$ROOTDIR/zstd/lib/zstd.h" "$ROOTDIR/out/$ZIG_TARGET/include/zstd.h"
+cd "$ROOTDIR/out/$ZIG_TARGET/lib"
 $ZIG build-lib \
   --name zstd \
-  -target $TARGET \
-  -mcpu=$MCPU \
+  -target $ZIG_TARGET \
+  -mcpu=baseline
   -fstrip -OReleaseFast \
   -lc \
   $(find "$ROOTDIR/zstd/lib" -type f \( -name "*.c" -o -name "*.S" \))
 
-mkdir -p "$ROOTDIR/out/build-llvm-$TARGET-$MCPU"
-cd "$ROOTDIR/out/build-llvm-$TARGET-$MCPU"
+mkdir -p "$ROOTDIR/out/build-llvm-$ZIG_TARGET"
+cd "$ROOTDIR/out/build-llvm-$ZIG_TARGET"
 cmake "$ROOTDIR/llvm" \
-  -DCMAKE_INSTALL_PREFIX="$ROOTDIR/out/$TARGET-$MCPU" \
-  -DCMAKE_PREFIX_PATH="$ROOTDIR/out/$TARGET-$MCPU" \
+  -DCMAKE_INSTALL_PREFIX="$ROOTDIR/out/$ZIG_TARGET" \
+  -DCMAKE_PREFIX_PATH="$ROOTDIR/out/$ZIG_TARGET" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_CROSSCOMPILING=True \
   -DCMAKE_SYSTEM_NAME="$TARGET_OS_CMAKE" \
-  -DCMAKE_C_COMPILER="$ZIG;cc;-fno-sanitize=all;-s;-target;$TARGET;-mcpu=$MCPU" \
-  -DCMAKE_CXX_COMPILER="$ZIG;c++;-fno-sanitize=all;-s;-target;$TARGET;-mcpu=$MCPU" \
-  -DCMAKE_ASM_COMPILER="$ZIG;cc;-fno-sanitize=all;-s;-target;$TARGET;-mcpu=$MCPU" \
+  -DCMAKE_C_COMPILER="$TOOLCHAIN/bin/cc" \
+  -DCMAKE_CXX_COMPILER="$TOOLCHAIN/bin/c++" \
+  -DCMAKE_ASM_COMPILER="$TOOLCHAIN/bin/c++" \
+  -DCMAKE_C_FLAGS="-fno-sanitize=all -s -mcpu=baseline"
+  -DCMAKE_CXX_FLAGS="-fno-sanitize=all -s -mcpu=baseline"
   -DCMAKE_LINK_DEPENDS_USE_LINKER=OFF \
-  -DCMAKE_RC_COMPILER="$ROOTDIR/out/host/bin/llvm-rc" \
-  -DCMAKE_AR="$ROOTDIR/out/host/bin/llvm-ar" \
-  -DCMAKE_RANLIB="$ROOTDIR/out/host/bin/llvm-ranlib" \
+  -DCMAKE_RC_COMPILER="$TOOLCHAIN/bin/rc" \
+  -DCMAKE_AR="$TOOLCHAIN/bin/ar" \
+  -DCMAKE_RANLIB="$TOOLCHAIN/bin/ranlib"
   -DLLVM_ENABLE_BACKTRACES=OFF \
   -DLLVM_ENABLE_BINDINGS=OFF \
   -DLLVM_ENABLE_LIBEDIT=OFF \
@@ -69,19 +74,17 @@ cmake "$ROOTDIR/llvm" \
   -DLLVM_ENABLE_ZLIB=FORCE_ON \
   -DLLVM_ENABLE_ZSTD=FORCE_ON \
   -DLLVM_USE_STATIC_ZSTD=ON \
-  -DLLVM_TABLEGEN="$ROOTDIR/out/host/bin/llvm-tblgen" \
-  -DCLANG_TABLEGEN="$ROOTDIR/out/build-llvm-host/bin/clang-tblgen" \
-  -DLLVM_DEFAULT_TARGET_TRIPLE="$TARGET"
+  -DLLVM_DEFAULT_TARGET_TRIPLE="$ZIG_TARGET"
 cmake --build . --target install
 
 cd "$ROOTDIR/zig"
 $ZIG build \
-  --prefix "$ROOTDIR/out/zig-$TARGET-$MCPU" \
-  --search-prefix "$ROOTDIR/out/$TARGET-$MCPU" \
+  --prefix "$ROOTDIR/out/zig-$ZIG_TARGET" \
+  --search-prefix "$ROOTDIR/out/$ZIG_TARGET" \
   -Dflat \
   -Dstatic-llvm \
   -Doptimize=ReleaseFast \
   -Dstrip \
-  -Dtarget="$TARGET" \
-  -Dcpu="$MCPU" \
+  -Dtarget="$ZIG_TARGET" \
+  -Dcpu="baseline" \
   -Dversion-string="$ZIG_VERSION"
